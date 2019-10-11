a4a643b7ab7ee1a023f78bf35c731b31e2531611
Cheng-Zhen Yang <chengzhenyang@gmail.com>
2019-10-11 22:46:03 +1300

    lab11

--- lab11/ex2/ex2.c
+++ lab11/ex2/ex2.c
@@ -7,7 +7,6 @@
 
 #define MAX_VALUES 1000
 
-
 // This program prompts the user to enter a series of integer values, terminated by the
 // value -1.  It then generates an output file which contains all entered values, space
 // separated, sorted in decreasing order
@@ -19,31 +18,40 @@ int main(void)
 	int value = 0;
 	FILE *fp;
 
-
 	// Open the output file for writing
-
-
-
-
+	fp = fopen("output.txt", "w");
 
 	// Read all input values from the user, until the terminating -1
-
-
-
-
+	printf("Enter positive integers (-1 to end): ");
+	scanf("%d", &value);
+	while (value != -1)
+	{
+		values[numValues] = value;
+		scanf("%d", &value);
+		numValues++;
+	}
 
 	// Sort the values into decreasing order (see the Lab Preparation exercises)
-
-
-
-
-
+	int a;
+	for (int i = 0; i < numValues; i++)
+	{
+		for (int j = i + 1; j < numValues; j++)
+		{
+			if (values[i] < values[j])
+			{
+				a = values[i];
+				values[i] = values[j];
+				values[j] = a;
+			}
+		}
+	}
 
 	// Write the values to file (and close the file using fclose(fp))
-
-
-
+	for (int i = 0; i < numValues; i++)
+	{
+		fprintf(fp, "%d ", values[i]);
+	}
+	fclose(fp);
 
 	return 0;
 }
-
